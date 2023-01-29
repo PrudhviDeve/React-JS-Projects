fb4fa131c824c052bb9bdba15a82025640abae2e
PRUDHVIDHAR RATHNA <rathna.prudhvi.6180@gmail.com>
2023-01-30 00:04:04 +0530

    adding projects

---
+++ quizapp/src/Helpers/QuestionBank.js
@@ -0,0 +1,36 @@
+export const Questions = [
+    {
+        prompt: "What is 1 + 3?",
+        optionA: "24",
+        optionB: "21",
+        optionC: "12",
+        optionD: "4",
+        answer: "D"
+    },
+    {
+        prompt: "Who is PM of India?",
+        optionA: "Rahul Gandhi",
+        optionB: "Sonia Gandhi",
+        optionC: "Amit Shah",
+        optionD: "Narendra Modi",
+        answer: "D"
+
+    },
+    {
+        prompt: "What is full name of Kohli?",
+        optionA: "Virat kohli",
+        optionB: "MS Kohli",
+        optionC: "Kohli Sharma",
+        optionD: "KL Kohli",
+        answer: "A"
+
+    },
+    {
+        prompt: "What is the Capital of India?",
+        optionA: "Mumbai",
+        optionB: "Chennai",
+        optionC: "New Delhi",
+        optionD: "Hyderabad",
+        answer: "C"
+    },
+]
